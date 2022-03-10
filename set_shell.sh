14735c50c04c0c6fad676fb923de2954744285d2
chengyue <yue.cheng.ted@gmail.com>
2022-03-10 09:23:03 +0000

    1

--- set_shell.sh
+++ set_shell.sh
@@ -4,3 +4,6 @@ set -e
 dircolors -p > ~/.dir_colors
 echo "alias ls='ls -F --show-control-chars --color=auto'
 eval \`dircolors -b ~/.dir_colors\`" >> ~/.bashrc
+
+echo "export PS1='\[\e]0;\u@\h: \w\a\]\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\\$ '" >> ~/.bashrc
+echo "需要手动调用'hostname 机器ip'，来指定显示名称"
