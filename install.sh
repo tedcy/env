9e4ba547619e4d20d73e5f800add040ed66dbd99
chengyue <yue.cheng.ted@gmail.com>
2022-05-10 11:34:57 +0800

    1

--- install.sh
+++ install.sh
@@ -7,28 +7,65 @@ rm -rf ~/.vim/bundle/*
 mkdir -pv ~/.vim/bundle
 
 if [ -f "vim_download.tar.gz" ];then
-    tar xzf vim_download.tar.gz
+    if [ ! -d "vim_download" ];then
+        tar xzf vim_download.tar.gz
+    fi
     cp -r vim_download/* ~/.vim/bundle/
 fi
 
 git config --global http.sslVerify false
 
-apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev --allow-unauthenticated
-
 #python3.8
-chmod 1777 /tmp
-cp sources.list /etc/apt/sources.list
-apt update
-apt-get install -y software-properties-common --allow-unauthenticated
-add-apt-repository -y ppa:deadsnakes/ppa | true
-apt update
-apt-get install -y python3.8 libpython3.8-dev --allow-unauthenticated
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-if [ ! -f "get-pip.py" ];then
-    wget https://bootstrap.pypa.io/get-pip.py
+#chmod 1777 /tmp
+#cp sources.list /etc/apt/sources.list
+#apt update
+#apt-get install -y software-properties-common --allow-unauthenticated
+#add-apt-repository -y ppa:deadsnakes/ppa | true
+#apt update
+#apt-get install -y python3.8 libpython3.8-dev --allow-unauthenticated
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+#if [ ! -f "get-pip.py" ];then
+#    wget https://bootstrap.pypa.io/get-pip.py
+#fi
+##python3.5
+##if [ ! -f "get-pip.py" ];then
+##    wget https://bootstrap.pypa.io/pip/3.5/get-pip.py
+##fi
+#python3 get-pip.py
+
+rm -rf openssl-1.1.1d
+rm -rf /root/openssl
+if [ ! -d "vim_download" ];then
+    wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz
+    tar xvf openssl-1.1.1d.tar.gz
+    rm -rf openssl-1.1.1d.tar.gz
+else
+    cp -r vim_download/openssl-1.1.1d .
 fi
-python3 get-pip.py
+cd openssl-1.1.1d
+./config --prefix=/root/openssl --openssldir=/root/openssl
+make -j 20
+make install
+cd -
+
+apt-get install -y libffi-dev --allow-unauthenticated
+rm -rf Python-3.8.12
+if [ ! -d "vim_download" ];then
+    wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tar.xz
+    tar xvf Python-3.8.12.tar.xz
+    rm -rf Python-3.8.12.tar.xz
+else
+    cp -r vim_download/Python-3.8.12 .
+fi
+cd Python-3.8.12
+./configure --prefix=/usr/local --with-openssl=/root/openssl --enable-shared
+make -j 20
+make install
+cd -
+update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.8 2
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+rm /usr/bin/lsb_release
 
 #see here https://github.com/Valloric/YouCompleteMe#linux-64-bit
 if [ ! `which gcc` ];then
@@ -36,6 +73,10 @@ if [ ! `which gcc` ];then
 fi
 
 #vim
+rm -rf vim
+#apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev \
+#    libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev --allow-unauthenticated
+apt-get install -y libncurses5-dev libbonoboui2-dev --allow-unauthenticated
 if [ ! -d "vim_download" ];then
     git clone https://github.com/vim/vim.git
 else
@@ -50,7 +91,7 @@ git checkout v8.2.4897
 	--enable-perlinterp=yes \
 	--enable-luainterp=yes \
 	--prefix=/usr/local
-make -j 12
+make -j 20
 make install
 cd -
 
