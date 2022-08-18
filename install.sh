b9aae939efe89784699807b8030973679a06cd8d
chengyue <yue.cheng.ted@gmail.com>
2022-08-18 20:47:18 +0800

    1

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ rm -rf ~/.vim/bundle/*
 mkdir -pv ~/.vim/bundle
 
 if [ -f "vim_download.tar.gz" ];then
-    if [ `grep -c "file:///" ~/.vimrc` == 0 ];then
+    if ! grep -q "file:///" ~/.vimrc;then
         sed -i "s:Plugin.*/:Plugin 'file\:///root/.vim/bundle/:" `find vimrc -name "*.vimrc"`
     fi
     if [ ! -d "vim_download" ];then
