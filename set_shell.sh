7b4d863a97d4e1b736e554187c0f5a7dbe6d7c08
chengyue <yue.cheng.ted@gmail.com>
2022-09-02 10:20:11 +0800

    1

--- set_shell.sh
+++ set_shell.sh
@@ -7,4 +7,5 @@ eval \`dircolors -b ~/.dir_colors\`" >> ~/.bashrc
 
 echo "export PS1='\[\e]0;\u@\h: \w\a\]\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\\$ '" >> ~/.bashrc
 echo "alias gg='g++ -std=c++11 -o out.exe -g -pthread'" >> ~/.bashrc
+echo "alias ggd='g++ -std=c++11 -o out.exe -g -pthread -D LOG_TAG'" >> ~/.bashrc
 echo "需要手动调用'hostname 机器ip'，来指定显示名称"
