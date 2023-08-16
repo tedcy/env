d79f89141242fc941c6301fb9918593d3f9733a0
chengyue <yue.cheng.ted@gmail.com>
2023-08-16 15:18:58 +0800

    add clang_make

---
+++ clang_make/clang-make.sh
@@ -0,0 +1,76 @@
+set -x
+
+isAnalyzerMode=0
+isBearMode=0
+for arg in "$@"
+do
+    if [ "$arg" == "analyzer" ]; then
+        isAnalyzerMode=1
+    fi
+    if [ "$arg" == "bear" ]; then
+        isBearMode=1
+    fi
+done
+
+if [ "$isAnalyzerMode" -eq 1 ]; then
+    echo "analyze mode enable"
+fi
+if [ "$isBearMode" -eq 1 ]; then
+    echo "bear mode enable"
+fi
+
+default_CFLAGS="-ftime-trace -fno-access-control -Wno-unused-command-line-argument -Wno-unknown-warning-option"
+
+#default_CFLAGS+=" -Wno-everything"
+#taf warning
+default_CFLAGS+=" -Wno-unused-lambda-capture"
+default_CFLAGS+=" -Wno-inconsistent-missing-override"
+default_CFLAGS+=" -Wno-braced-scalar-init"
+default_CFLAGS+=" -Wno-mismatched-tags"
+
+#brpc warning
+default_CFLAGS+=" -Wno-header-guard"
+default_CFLAGS+=" -Wno-overloaded-virtual"
+
+#other
+default_CFLAGS+=" -Wno-delete-non-abstract-non-virtual-dtor"
+
+default_Clang14=${default_Clang14:-"/root/.vim/bundle/YouCompleteMe/clang+llvm-14.0.0-x86_64-unknown-linux-gnu/bin/clang++"}
+curPath=$(pwd)
+
+if [ "$isAnalyzerMode" -eq 1 ]; then
+    ClangBuildAnalyzerPath="/root/env/ClangBuildAnalyzer/ClangBuildAnalyzer"
+    if [ ! -d $ClangBuildAnalyzerPath ];then
+        git clone https://github.com/aras-p/ClangBuildAnalyzer $ClangBuildAnalyzerPath
+    fi
+    ClangBuildAnalyzerBinPath="$ClangBuildAnalyzerPath/build/ClangBuildAnalyzer"
+    if [ ! -f $ClangBuildAnalyzerBinPath ];then
+        mkdir -pv $ClangBuildAnalyzerPath/build
+        cd $ClangBuildAnalyzerPath/build
+        cd build
+        cmake ..
+        make -j
+        cd -
+    fi
+    $ClangBuildAnalyzerBinPath --stop $curPath /dev/null
+    $ClangBuildAnalyzerBinPath --start $curPath
+fi
+
+if [ -f "make_flags.config" ];then
+    # 如果文件存在，查找 CFLAGS 关键词
+    make_flags_CFLAGS=$(grep "CFLAGS" make_flags.config | sed 's/CFLAGS=//'| sed 's/CFLAGS =//'| tr -d '"')
+    make_flags_other=$(grep -v "CFLAGS" make_flags.config)
+fi
+    
+# 执行 make 命令并添加文件内容
+if [ "$isBearMode" -eq 1 ]; then
+    bear make -j CFLAGS="$default_CFLAGS $make_flags_CFLAGS" CXX="$default_Clang14" $make_flags_other && cp compile_commands.json compile_commands.json_bk && compdb -p . list > /tmp/compile_commands.json && cp /tmp/compile_commands.json .
+else
+    make -j CFLAGS="$default_CFLAGS $make_flags_CFLAGS" CXX="$default_Clang14" $make_flags_other
+fi
+
+if [ "$isAnalyzerMode" -eq 1 ]; then
+    now=$(date "+%Y-%m-%d-%H-%M-%S")
+    $ClangBuildAnalyzerBinPath --stop $curPath /tmp/ClangBuildAnalyzer.file_$now
+    $ClangBuildAnalyzerBinPath --analyze /tmp/ClangBuildAnalyzer.file_$now > /tmp/ClangBuildAnalyzer.file_$now.result
+fi
